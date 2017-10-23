532e131c70d7496575d6f5d4bd458c0f39857186
Bowers <W0303446@campus.nscc.ca>
2017-10-23 18:36:22 -0300

    Initial commit

---
+++ app/src/main/java/w0303446/quizbuilder/homebase.java
@@ -0,0 +1,13 @@
+package w0303446.quizbuilder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class homebase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homebase);
+    }
+}
